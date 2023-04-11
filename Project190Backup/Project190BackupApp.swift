b913ebd1d3b20bbf00888ce421cbe98b796cf11d
BaconToasts <vlad.puriy@gmail.com>
2023-04-10 21:47:48 -0700

    Initial Commit

---
+++ Project190Backup/Project190BackupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project190BackupApp.swift
+//  Project190Backup
+//
+//  Created by Vlad Puriy on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Project190BackupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
