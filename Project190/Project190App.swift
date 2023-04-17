8759b5bc24f71f6a52f286283f207bca833ba940
BaconToasts <vlad.puriy@gmail.com>
2023-04-17 13:39:58 -0700

    Changed name from Project190Backup -> Project190

--- Project190/Project190App.swift
+++ Project190/Project190App.swift
@@ -1,6 +1,6 @@
 //
-//  Project190BackupApp.swift
-//  Project190Backup
+//  Project190App.swift
+//  Project190
 //
 //  Created by Vlad Puriy on 4/10/23.
 //
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct Project190BackupApp: App {
+struct Project190App: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
