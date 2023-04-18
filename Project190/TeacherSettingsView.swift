530e4aaa9d51428a23270c55432d3008c502770d
lukelobo <la.campos2510@gmail.com>
2023-04-18 12:03:43 -0700

    C1PL - 96 I created a page where teachers can alter certain settings and made it connected with the main teacher view

---
+++ Project190/TeacherSettingsView.swift
@@ -0,0 +1,57 @@
+//
+//  TeacherSettingsView.swift
+//  Project190
+//
+//  Created by Luis Campos on 4/18/23.
+//
+
+import SwiftUI
+struct TeacherSettingsView: View{
+    @Binding var showNextView: DisplayState
+    
+    @State var firstButton = "Main/Settings"
+    @State var secondButton = "Change Password"
+    @State var thirdButton = "Enable FaceID"
+    @State var fourthButton = "Disable Bluetooth"
+    @State var borderColor = Color.black
+    @State var mainButtonColor = Color.black
+    
+    var body: some View{
+        VStack{
+            Button(action:{withAnimation{showNextView = .mainTeacher}}){
+                Text(firstButton).fontWeight(.bold).foregroundColor(.white).frame(width: 300, height: 20, alignment:.center)
+            }
+            .padding()
+            .background(mainButtonColor)
+            .cornerRadius(100)
+            .padding(.bottom, 200)
+            Button(action:{}){
+                Text(secondButton).fontWeight(.bold).foregroundColor(.black).frame(width: 300, height: 20, alignment:.center)
+            }
+            .padding()
+            .border(mainButtonColor, width: 5)
+            .cornerRadius(6)
+            .padding(.bottom, 10)
+            Button(action:{}){
+                Text(thirdButton).fontWeight(.bold).foregroundColor(.black).frame(width: 300, height: 20, alignment:.center)
+            }
+            .padding()
+            .border(mainButtonColor, width: 5)
+            .cornerRadius(6)
+            .padding(.bottom, 10)
+            Button(action:{}){
+                Text(fourthButton).fontWeight(.bold).foregroundColor(.black).frame(width: 300, height: 20, alignment:.center)
+            }
+            .padding()
+            .border(mainButtonColor, width: 5)
+            .cornerRadius(6)
+            .padding(.bottom, 300)
+        }
+    }
+}
+struct TeacherSettingsView_Previews: PreviewProvider{
+    @State static private var showNextView: DisplayState = .teacherSettings
+    static var previews: some View{
+        TeacherSettingsView(showNextView: $showNextView)
+    }
+}
