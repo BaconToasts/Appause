9999c4f69c900c071a5fc5bcbe57ece28397d85b
TheWizland <tkabanets@hotmail.com>
2023-04-19 20:33:59 -0700

    C1PL-100: StudentChooseAdminView now connects to StudentAppRequestView.

--- Project190/StudentChooseAdminView.swift
+++ Project190/StudentChooseAdminView.swift
@@ -15,33 +15,32 @@ struct StudentChooseAdminView: View {
     ]
     
     var body: some View {
-        VStack {
-            Text("Main / Requests")
-                .padding()
-                .background(Color.black)
-                .foregroundColor(.white)
-                .cornerRadius(5)
-                .padding(.top, -30)
-                .padding(.bottom, 40)
+        NavigationView {
+            VStack {
+                Text("Main / Requests")
+                    .padding()
+                    .background(Color.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(5)
+                    .padding(.top, -30)
+                    .padding(.bottom, 40)
                 
                 
-            
-            List {
-                ForEach(adminList, id:\.self) { admin in
-                    VStack{
-                        Button(action: {}) {
+                List {
+                    ForEach(adminList, id:\.self) { admin in
+                        NavigationLink(destination: StudentAppRequestView(adminName: admin)) {
                             Text(admin)
-                                .padding(15)
-                                .font(.system(size:35))
-                                .foregroundColor(.black)
                         }
+                        .padding(10)
+                        .font(.system(size:30))
+                        .foregroundColor(.black)
                     }
                 }
-            }
-            .overlay(RoundedRectangle(cornerRadius:10, style:.circular)
-                .stroke(lineWidth:3))
+                .overlay(RoundedRectangle(cornerRadius:10, style:.circular)
+                    .stroke(lineWidth:3))
                 .frame(maxWidth: UIScreen.main.bounds.size.width*0.85,
                        maxHeight: UIScreen.main.bounds.size.height*0.7)
+            }
         }
     }
 }
