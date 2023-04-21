3e23e3228f00d2ed5942b5cfe852a8be2021d7fa
Alec L <57418300+Wamuu-21@users.noreply.github.com>
2023-04-20 22:34:25 -0700

    Update StudentMainView.swift
    adjusted GUI

--- Project190/StudentMainView.swift
+++ Project190/StudentMainView.swift
@@ -12,31 +12,39 @@ struct StudentMainView: View {
     @Binding var showNextView: DisplayState
     
     @State var mainButtonColor = Color.black
+    @State private var btnColor: Color = Color.gray.opacity(0.25)
     @State var secondButtonName = "REQUESTS"
     @State var thirdButtonName = "CONNECT CODE"
     @State var fourthButtonName = "SETTINGS"
     @State var fifthButtonName = "MANAGE ADMIN"
     @State var sixthButtonName = "Logout"
+    @State private var cornerRadius: CGFloat = 6
+    @State private var frameWidth: CGFloat = 300
+    @State private var frameHeight: CGFloat = 20
     
     var body: some View {
         VStack {
             Button(action:{}){
-                Text("MAIN").foregroundColor(.white).fontWeight(.bold).frame(width:300, height: 20, alignment: .center)
+                Text("MAIN").foregroundColor(.white).fontWeight(.bold).frame(width:frameWidth, height: frameHeight, alignment: .center)
             }
             .padding()
             .background(mainButtonColor)
+            .border(Color.black, width: 5)
             .cornerRadius(100)
-            .padding(.bottom, 50)
+            .padding(.bottom, 30)
             Button(action:{}){
                 Text(secondButtonName)
+                    .padding(.leading, 25)
                     .foregroundColor(.black)
-                    .frame(width: 300, height: 20, alignment: .center)
+                    .frame(width: frameWidth, height: frameHeight, alignment: .center)
                     .fontWeight(.bold)
                 Image(systemName: "hand.raised")
+                    .fontWeight(.bold)
                     .imageScale(.large)
                     .foregroundColor(.black)
             }
             .padding()
+            .background(btnColor)
             .border(Color.black, width: 5)
             .cornerRadius(6)
             .padding(.bottom, 10)
@@ -47,26 +55,30 @@ struct StudentMainView: View {
                     showNextView = .studentConnectCode}
             }){
                 Text(thirdButtonName)
+                    .padding(.leading, 25)
                     .foregroundColor(.black)
-                    .frame(width: 335, height: 30, alignment: .center)
+                    .frame(width: 335, height: frameHeight, alignment: .center)
                     .fontWeight(.bold)
             }
             .padding()
+            .background(btnColor)
             .border(Color.black, width: 5)
-            .cornerRadius(6)
+            .cornerRadius(cornerRadius)
             .padding(.bottom, 10)
             Button(action: {withAnimation {showNextView = .studentSettings}}){
                 Text(fourthButtonName)
+                    .padding(.leading, 25)
                     .foregroundColor(.black)
-                    .frame(width: 300, height: 20, alignment: .center)
+                    .frame(width: frameWidth, height: frameHeight, alignment: .center)
                     .fontWeight(.bold)
                 Image(systemName: "gear")
                     .imageScale(.large)
                     .foregroundColor(.black)
             }
             .padding()
+            .background(btnColor)
             .border(Color.black, width: 5)
-            .cornerRadius(6)
+            .cornerRadius(cornerRadius)
             .padding(.bottom, 10)
                         
             Button(action: {}){
@@ -76,8 +88,9 @@ struct StudentMainView: View {
                     .fontWeight(.bold)
             }
             .padding()
+            .background(btnColor)
             .border(Color.black, width: 5)
-            .cornerRadius(6)
+            .cornerRadius(cornerRadius)
             .padding(.bottom, 265)
             Button(action: {
                 withAnimation {
