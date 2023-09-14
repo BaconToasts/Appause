99bec407677d71174d22361b66af74cc1389da75
TheWizland <tkabanets@hotmail.com>
2023-09-14 03:18:19 -0700

    StudentChooseAdminView now connects to StudentConnectCodeView. Redid appearance of StudentChooseAdminView to be closer to the Figma diagram.

--- Project190/StudentChooseAdminView.swift
+++ Project190/StudentChooseAdminView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct StudentChooseAdminView: View {
     //Add this binding state for transitions from view to view
     @Binding var showNextView: DisplayState
@@ -39,31 +40,46 @@ struct StudentChooseAdminView: View {
                 
                 ScrollView {
                     ForEach(adminList, id:\.self) { admin in
-                        HStack{
-                            NavigationLink("-", destination: StudentDeleteAdminView(adminName:admin)
+                        ZStack {
+                            NavigationLink("-", destination: StudentDeleteAdminView(adminName: admin)
                                 .navigationBarHidden(true))
                                 .font(.system(size:30))
+                                .frame(maxWidth:.infinity, alignment:.leading)
+                                .padding(.leading, 5)
                                 .foregroundColor(.black)
-                            NavigationLink(admin, destination: StudentAppRequestView(adminName: admin)
-                                .navigationBarHidden(true))
-                                .padding(10)
+                            
+                            Text(admin)
+                                .font(.system(size:25))
+                                .multilineTextAlignment(.center)
+                                .frame(maxWidth:.infinity, alignment:.center)
+                                .foregroundColor(.black)
+                            
+                            NavigationLink(destination: StudentAppRequestView(adminName: admin)
+                                .navigationBarHidden(true)) {
+                                Image(systemName: "hand.raised.fill")
+                                    .foregroundColor(Color.yellow)
+                            }
+                                .padding(.trailing, 5)
+                                .frame(maxWidth:.infinity, alignment:.trailing)
                                 .font(.system(size:30))
-                                .foregroundColor(.black)
-                                .navigationBarHidden(true)
                         }
-                        .frame(maxWidth: .infinity)
+                            .padding(5)
                     }
                     
-                    NavigationLink(destination: StudentConnectCodeView()) {
-                        HStack{
+                    NavigationLink(destination: StudentConnectCodeView()
+                        .navigationBarHidden(true)) {
+                        ZStack{
                             Text("+")
+                                .font(.system(size:30))
+                                .frame(maxWidth:.infinity, alignment:.leading)
+                                .padding(.leading, 5)
                             Text("New Class")
+                                .frame(maxWidth:.infinity, alignment:.center)
+                                .font(.system(size:25))
                         }
-                        .padding(10)
-                        .font(.system(size:30))
-                        .foregroundColor(.black)
+                            .padding(5)
+                            .foregroundColor(.black)
                     }
-                    .navigationBarHidden(true)
                 }
                 .overlay(RoundedRectangle(cornerRadius:6, style:.circular)
                     .stroke(lineWidth:3))
@@ -71,7 +87,6 @@ struct StudentChooseAdminView: View {
                        maxHeight: UIScreen.main.bounds.size.height*0.7)
             }
         }
-            .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
