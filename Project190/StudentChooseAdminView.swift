13fd43ff5c461a7dc81e6da08679bde820490d6a
NavBernal <navjit.bernal@gmail.com>
2023-09-17 18:24:50 -0700

    # C1PL-105: Cleaned up the Student Choose Admin View.

--- Project190/StudentChooseAdminView.swift
+++ Project190/StudentChooseAdminView.swift
@@ -29,14 +29,17 @@ struct StudentChooseAdminView: View {
                 })
                 {
                     Text("MAIN / CLASSES")
-                        .padding()
-                        .background(Color.black)
+                        .fontWeight(btnStyle.getFont())
                         .foregroundColor(.white)
-                        .cornerRadius(5)
-                        .padding(.top, -30)
-                        .padding(.bottom, 30)
-                    
+                        .frame(width: btnStyle.getWidth(),
+                               height: btnStyle.getHeight(),
+                               alignment: btnStyle.getAlignment())
+                       
                 }
+                .padding()
+                .background(btnStyle.getPathColor())
+                .cornerRadius(btnStyle.getPathRadius())
+                .padding(.bottom, 70)
                 
                 ScrollView {
                     ForEach(adminList, id:\.self) { admin in
