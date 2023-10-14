eb0d5f04d5bdc32795c69d4bd37453e0e288a64e
BaconToasts <Vlad.puriy@gmail.com>
2023-10-13 22:00:18 -0700

    Connected Reset password pages.

--- Project190/EmailCodeView.swift
+++ Project190/EmailCodeView.swift
@@ -31,7 +31,7 @@ struct EmailCodeView: View {
                 print("Button was tapped")
                 withAnimation {
                     //show nextView .whateverViewYouWantToShow defined in ContentView Enum
-                    showNextView = .resetPassword
+                    showNextView = .pwCodeVerification
                 }
                 email = email.trimmingCharacters(in: .whitespacesAndNewlines)
                 if isValidEmail(email) {
@@ -75,10 +75,10 @@ struct EmailCodeView: View {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("api-x", forHTTPHeaderField: "Key") // Replace with your API key
+        request.addValue("api-F199C926535F11EE96AEF23C91C88F4E", forHTTPHeaderField: "Key") // Replace with your API key
 
         let body: [String: Any] = [
-            "api_key": "api-x", // Your API key
+            "api_key": "api-F199C926535F11EE96AEF23C91C88F4E", // Your API key
             "to": ["<\(email)>"], // The recipient's email address, formatted correctly
             "sender": "appaused.service@gmail.com", // Your email address
             "subject": "Your Verification Code",
