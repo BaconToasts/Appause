6be02bb18b98440248c75eeeed909618b206ffda
Ranger-72 <vanguard4forall@gmail.com>
2023-10-27 14:54:56 -0700

    # C1PL-134
    # Added more passwords to password array for Unit testing script for Password Requirements

--- Project190Tests/PasswordRequirementsTests.swift
+++ Project190Tests/PasswordRequirementsTests.swift
@@ -2,7 +2,7 @@
 //  PasswordRequirementsTests.swift
 //  Project190Tests
 //
-//  Created by user on 10/26/23.
+//  Created by Eduardo Mariano on 10/26/23.
 //
 
 import XCTest
@@ -12,10 +12,11 @@ final class PasswordRequirementsTests: XCTestCase {
 
     func testPasswordRequirement() {
         // test passwords
-        let passwords = ["Ch3rry!", "123", "fgh2", "d3ltaF05ce", "jashdj!shdfk", "Whyi5thisaPass"]
+        let passwords = ["Ch3rry!", "123", "fgh2", "d3ltaF05ce", "jashdj!shdfk", "Whyi5thisaPass", "project!90", "etst7", "8ill0t"]
         let valid = Validate()
         
         // loop through passwords array
+        print("\n")
         for password in passwords {
             let check = valid.validatePassword(password)
             if (check == false){
